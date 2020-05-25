a356e5714b0d0909ec8212768b5f46f444d616bd
QuintyHH <razmocanu@gmail.com>
2020-05-25 20:06:48 +0100

    Updates

---
+++ precache-manifest.8b88c4004f9757c295caae83add26596.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bcc723c4301c9dde780a37e8334a62a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1afbafdcebdde4dbbf84",
+    "url": "/static/css/main.c6353f6c.chunk.css"
+  },
+  {
+    "revision": "6dd8f694c71b52fba15e",
+    "url": "/static/js/2.38393d9b.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/static/js/2.38393d9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1afbafdcebdde4dbbf84",
+    "url": "/static/js/main.30efe48d.chunk.js"
+  },
+  {
+    "revision": "826a09d9f50fb7f61d94",
+    "url": "/static/js/runtime-main.8c470a90.js"
+  }
+]);
